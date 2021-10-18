261606bcfb64e2de821c9d0f446bce4a0d7dccd3
Luigi Cruz <luigifcruz@gmail.com>
2021-10-17 22:05:53 -0300

    change theme

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ exclude_patterns = ['_build', 'Thumbs.db', 'tests', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx-rtd-theme'
+html_style = '/default.css'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
